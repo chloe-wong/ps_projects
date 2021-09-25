6673fd183d2c2efc39a95f828c56d35489318add
Chloe Wong <66992547+chloe-wong@users.noreply.github.com>
2021-09-26 04:00:32 +0800

    Update build_model.py

--- emotionclassifier/build_model.py
+++ emotionclassifier/build_model.py
@@ -16,7 +16,7 @@ picture_size = 48
 batch_size = 256
 def load_train(folder_path):
     datagen_train = ImageDataGenerator
-    train_set = datagen_train.flow_from_directory(folder_path + "/train",
+    train_set = datagen_train.flow_from_directory(directory = folder_path + "/train",
                                                   target_size=(picture_size, picture_size),
                                                   color_mode="grayscale",
                                                   batch_size=batch_size,
@@ -26,93 +26,93 @@ def load_train(folder_path):
 
 def load_test(folder_path):
     datagen_val = ImageDataGenerator()
-    test_set = datagen_val.flow_from_directory(folder_path+"/validation",
+    test_set = datagen_val.flow_from_directory(directory = folder_path+"/validation",
                                                   target_size = (picture_size,picture_size),
                                                   color_mode = "grayscale",
                                                   batch_size=batch_size,
                                                   class_mode='categorical',
                                                   shuffle=False)
-
+    return(test_set)
 
 
 def build_model():
     no_of_classes = 7
     model = Sequential()
-    
+
     #CNN layer 1
     model.add(Conv2D(64,(3,3),padding = 'same',input_shape = (48,48,1)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size = (2,2)))
     model.add(Dropout(0.25))
-    
+
     #CNN layer 2
     model.add(Conv2D(128,(5,5),padding = 'same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size = (2,2)))
     model.add(Dropout (0.25))
-    
+
     #CNN layer 3
     model.add(Conv2D(512,(3,3),padding = 'same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size = (2,2)))
     model.add(Dropout (0.25))
-    
+
     #CNN layer 4
     model.add(Conv2D(512,(3,3), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
-    
+
     model.add(Flatten())
-    
+
     model.add(Dense(256))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dropout(0.25))
-    
+
     model.add(Dense(512))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dropout(0.25))
-    
-    model.add(Dense(no_of_classes, activation='softmax'))
-    
-    return model 
 
-def train_model(model,train_set,test_set): 
+    model.add(Dense(no_of_classes, activation='softmax'))
+
+    return model
+
+def train_model(model,train_set,test_set):
     opt = Adam(lr = 0.0001)
     model.compile(optimizer=opt,loss='categorical_crossentropy', metrics=['accuracy'])
     model.summary()
-    
+
     from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-    
+
     checkpoint = ModelCheckpoint("./model.h5", monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    
+
     early_stopping = EarlyStopping(monitor='val_loss',
                               min_delta=0,
                               patience=3,
                               verbose=1,
                               restore_best_weights=True
                               )
-    
+
     reduce_learningrate = ReduceLROnPlateau(monitor='val_loss',
                                   factor=0.2,
                                   patience=3,
                                   verbose=1,
                                   min_delta=0.0001)
-    
+
     callbacks_list = [early_stopping,checkpoint,reduce_learningrate]
-    
+
     epochs = 48
-    
+
     model.compile(loss='categorical_crossentropy',
                   optimizer = Adam(lr=0.001),
                   metrics=['accuracy'])
-    
+
     history = model.fit_generator(generator=train_set,
                                     steps_per_epoch=train_set.n//train_set.batch_size,
                                     epochs=epochs,
@@ -120,7 +120,7 @@ def train_model(model,train_set,test_set):
                                     validation_steps = test_set.n//test_set.batch_size,
                                     callbacks=callbacks_list
                                     )
-    
+
     return(history)
 
 def view_eval(history):
@@ -138,12 +138,3 @@ def view_eval(history):
     plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
     plt.legend(loc='lower right')
     plt.show()
-    
-
-
-if __name__ == "main":
-    train_set = load_train(folder_path)
-    test_set = load_test(folder_path)
-    model = build_model()
-    history = train_model(model,train_set,test_set)
-    view_eval(history)
